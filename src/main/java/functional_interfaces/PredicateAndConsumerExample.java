9e66b8288c60f6865d2a98741153dd6223aba476
DmitriyLakhno <dimalahno@rambler.ru>
2021-08-09 20:26:27 +0600

    Investigated BiPredicate

--- src/main/java/functional_interfaces/PredicateAndConsumerExample.java
+++ src/main/java/functional_interfaces/PredicateAndConsumerExample.java
@@ -3,6 +3,7 @@ package functional_interfaces;
 
 import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
@@ -15,12 +16,17 @@ public class PredicateAndConsumerExample {
     static Predicate<Student> gradleLevelFilter = student -> student.getGradeLevel()>=3;
     static Predicate<Student> gpaFilter = student -> student.getGpa()>=3.9;
 
+    static BiPredicate<Integer, Double> biPredicate = (gradeLevel, gpa) -> gradeLevel>=3 && gpa>=3.9;
+
     static BiConsumer<String, List<String>> biConsumer = (name, activities) -> {
         System.out.println(name + " : " + activities);
     };
 
     static Consumer<Student> studentConsumer = (student) -> {
-        if (gradleLevelFilter.and(gpaFilter).test(student)){
+//        if (gradleLevelFilter.and(gpaFilter).test(student)){
+//            biConsumer.accept(student.getName(), student.getActivities());
+//        }
+        if (biPredicate.test(student.getGradeLevel(), student.getGpa())){
             biConsumer.accept(student.getName(), student.getActivities());
         }
     };
